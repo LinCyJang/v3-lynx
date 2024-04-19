8bd53e52964771e36078592d78fec394a03d85fd
linxi <linxi.jiang@jeeyor.com>
2024-04-19 18:10:00 +0800

    验证工具函数

--- src/naive-ui.ts
+++ src/naive-ui.ts
@@ -1,7 +1,34 @@
-import { create, NButton } from 'naive-ui'
+import { create, NButton, NCard, NCarousel, NCheckbox, NCollapse, NCollapseItem, NDatePicker, NDialog, NDivider, NDropdown, NForm, NIcon, NInput, NInputNumber, NLayout, NLayoutSider, NMenu, NRadio, NSelect, NSkeleton, NSlider, NSpace, NSpin, NSwitch, NTab, NTabPane, NTabs, NTag } from 'naive-ui'
 
 export const navie = create({
   components: [
-    NButton
+    NButton,
+    NCard,
+    NTabs,
+    NTabPane,
+    NTab,
+    NForm,
+    NCarousel,
+    NCollapse,
+    NCollapseItem,
+    NDivider,
+    NDialog,
+    NDropdown,
+    NIcon,
+    NTag,
+    NCheckbox,
+    NDatePicker,
+    NInput,
+    NInputNumber,
+    NRadio,
+    NSelect,
+    NSkeleton,
+    NSlider,
+    NSpace,
+    NSpin,
+    NLayout,
+    NLayoutSider,
+    NMenu,
+    NSwitch
   ]
 })
